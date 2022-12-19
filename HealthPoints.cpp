2ca3ef0150d5b955322366b7ad9077133a2c3851
mariamassdi <117951268+mariamassdi@users.noreply.github.com>
2022-12-20 00:48:52 +0200

    Update HealthPoints.cpp

--- HealthPoints.cpp
+++ HealthPoints.cpp
@@ -1,9 +1,64 @@
+//
+// Created by USER on 19/12/2022.
+//
+
 #include "HealthPoints.h"
 
-HealthPoints::HealthPoints(int maxHP){
-  if(maxHP<=0){
-    throw HealthPoints:InvalidArgument();
-  }
-  m_HP=maxHp;
-  m_maxHP=maxHP;
+HealthPoints::HealthPoints(int max_hp ): m_max_hp(max_hp),m_hp(max_hp)
+{
+    if(max_hp<=0)
+    {
+    throw HealthPoints::InvalidArgument();
+    }
+
+}
+ bool operator==(const HealthPoints& h1, const HealthPoints& h2)
+{if(h1.m_hp==h2.m_hp)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool operator!=(const HealthPoints& h1,const HealthPoints& h2)
+{
+    if(h1==h2)
+    {
+        return false;
+    }
+    return true;
+}
+ bool operator<(const HealthPoints& h1, const HealthPoints& h2)
+{
+    if(h1.m_hp<h2.m_hp)
+    {
+        return true;
+    }
+return false;
+}
+
+bool operator<=(const HealthPoints& h1,const HealthPoints& h2)
+{
+    if(h1<h2||h1==h2)
+    {
+        return true;
+    }
+    return false;
+}
+bool operator>=(const HealthPoints& h1,const HealthPoints& h2)
+{
+    if(h1<h2)
+{
+        return false;
+}
+   return true;
+
+}
+bool operator>(const HealthPoints& h1,const HealthPoints& h2)
+{
+    if(h1<=h2)
+    {
+        return false;
+    }
+    return true;
 }
